15d82bc92b9eb2f204da709acad1fe02a45ef3ba
flyingshuriken <alvinchoi0623@gmail.com>
2026-03-15 14:38:39 +0800

    Initial Commit

---
+++ TinyQR/TinyQRApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinyQRApp.swift
+//  TinyQR
+//
+//  Created by Choi JS on 15/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct TinyQRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
